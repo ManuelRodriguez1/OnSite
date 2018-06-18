de7c526b5fcce84955497f14d45502eb87847a06
Manuel <manuel@nativo.design>
2018-06-18 11:09:27 -0400

    Agregados boton nuevo, funcionalidad y estilos de JoinPro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component ,OnInit} from '@angular/core';
-import * as $ from 'jquery';
+// import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,20 +10,20 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit() {  var concc=1;
-    $(document).ready(function(){
+    // $(document).ready(function(){
 
-          $(".btn111").click(function(){
-            if(concc==1){
-              concc=2;
-                $(".LoginCaja").removeClass("btnPrincipal2");
-                $(".LoginCaja").addClass("btnPrincipal1");
-            }else{
-                $(".LoginCaja").removeClass("btnPrincipal1");
-                $(".LoginCaja").addClass("btnPrincipal2");
-              concc=1;
-            }
+    //       $(".btn111").click(function(){
+    //         if(concc==1){
+    //           concc=2;
+    //             $(".LoginCaja").removeClass("btnPrincipal2");
+    //             $(".LoginCaja").addClass("btnPrincipal1");
+    //         }else{
+    //             $(".LoginCaja").removeClass("btnPrincipal1");
+    //             $(".LoginCaja").addClass("btnPrincipal2");
+    //           concc=1;
+    //         }
 
-          });
-      });
+    //       });
+    //   });
     }
 }
--- src/app/components/JoinPro/registro/reviews/reviews.component.ts
+++ src/app/components/JoinPro/registro/reviews/reviews.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
+  list = [
+    {"cliente": "Customer 1"},
+    {"cliente": "Customer 2"},
+    {"cliente": "Customer 3"},
+  ]
+  add = 4;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addc(){
+    this.list.push({"cliente":"Customer "+this.add});
+    this.add++;
   }
 
 }
