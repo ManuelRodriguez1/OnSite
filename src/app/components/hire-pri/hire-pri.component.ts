5b35d359bf6ca0c8e54cc336bd8390aaf7be2ea3
Unknown <katheryn@nativo.design>
2018-06-22 13:16:35 -0400

    Ventana modal hire

--- src/app/components/hire-pri/hire-pri.component.ts
+++ src/app/components/hire-pri/hire-pri.component.ts
@@ -9,7 +9,7 @@ declare var Swiper;
   templateUrl: './hire-pri.component.html',
   styleUrls: ['./hire-pri.component.css','../JoinPro/registro/select/select.component.css']
 })
-export class HirePriComponent implements AfterViewInit  {
+export class HirePriComponent  {
      //mySwiper = Swiper;
      menuVista="Home";
       Sesion = true;
@@ -17,6 +17,7 @@ export class HirePriComponent implements AfterViewInit  {
       aparecer = '';
       rotar = '';
       click = false;
+      mostrar1 = "mostrar";
 
       list = [
         {
@@ -25,13 +26,13 @@ export class HirePriComponent implements AfterViewInit  {
         {
           "categorias": "Decorator"
         },
-        {
+     {
           "categorias": "Drywall"
         },
         {
           "categorias": "Electrical"
         },
-        {
+        /*   {
           "categorias": "Excavation"
         },
         {
@@ -66,7 +67,7 @@ export class HirePriComponent implements AfterViewInit  {
         },
         {
           "categorias": "Waterproof Installation"
-        }
+        }*/
       ];
 
       constructor(public af: AngularFireAuth,private router: Router) {
@@ -80,24 +81,13 @@ export class HirePriComponent implements AfterViewInit  {
         });
        }
 
-         ngAfterViewInit() {
 
-           /*this.mySwiper =  new Swiper('.swiper-container', {
-              spaceBetween: 15,
-              direction: 'horizontal',
-              loop: true,
-              centeredSlides	:true,
-              slidesPerView: 3,
-              slideToClickedSlide	: true,
-              scrollbar: '.swiper-scrollbar',
-            });*/
-         }
 
 
 
   home(){
     this.menuVista="Home";
-  //   this.ngAfterViewInit();
+
   }
   inbox(){
     this.menuVista="Inbox";
@@ -124,4 +114,19 @@ export class HirePriComponent implements AfterViewInit  {
     }
   }
 
+  mostrar(t){
+    console.log(t);
+    if(this.mostrar1 == "nomostrar"){
+      this.mostrar1 = 'mostrar';
+    }else{
+      this.mostrar1 = 'nomostrar';
+    }
+  }
+nomostrar(){
+  if(this.mostrar1 == "nomostrar"){
+    this.mostrar1 = 'mostrar';
+  }else{
+    this.mostrar1 = 'nomostrar';
+  }
+}
 }
